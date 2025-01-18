498d121dcd328f1f11cbfad6b725a89a46a251ad
viksmol <leoniddemcuk403@gmail.com>
2025-01-18 11:37:59 +0200

    Add files via upload

--- igra.py
+++ igra.py
@@ -7,16 +7,23 @@ mixer.music.play()
 fire_sound = mixer.Sound('babax.ogg')
 
 font.init()
+font1 = font.Font(None,80)
 font2 = font.Font(None,36)
+win = font1.render('NAH ID WIN', True,  (255, 255, 255))
+lose = font1.render('You nod stronger', True, (180, 0, 0) )
+
 
 img_bacl = 'fon.jpg'
 img_hero = 'raleta.png'
+img_bullet = 'purpur.png'
 img_enemy = 'gojo.png'
 
 Clock = time.Clock()
 FPS = 60
+
 score = 0
 lost = 0
+max_lost = 3
 
 class GameSprite(sprite.Sprite):
     def __init__(self,  player_image, player_x, player_y, size_x, size_y, player_speed):
@@ -41,7 +48,22 @@ class Player(GameSprite):
             self.rect.x += self.speed
 
     def fire(self):
-        pass
+        bullet = Bullet(img_bullet, self.rect.centerx, self.rect.top, 15, 20, -15 )
+        bullets.add(bullet)
+class Bullet(GameSprite):
+    def update(self):
+        self.rect.y += self.speed
+        if self.rect.y < 0:
+            self.kill()
+    
+
+
+
+
+
+
+
+
 class Enemy(GameSprite):
     def update(self):
         self.rect.y += self.speed
@@ -65,6 +87,8 @@ for i in range(1, 6):
         80, win_width - 80),-40, 80, 50, randint(1,3))
     monsters.add(monster)
 
+bullets = sprite.Group()
+
 finish = False
 
 run =True
@@ -73,7 +97,11 @@ while run:
     for e in event.get():
         if e.type ==QUIT:
             run =False
-    
+
+        elif e.type == KEYDOWN:
+            if e.key == K_SPACE:
+                fire_sound.play()
+                ship.fire()
     if not finish:
         window.blit(backgoround, (0,0))
         text = font2.render("Рахунок:" + str(score), 1, (255, 255, 255))
@@ -82,11 +110,14 @@ while run:
         text_lose = font2.render(f"Пропущено: {lost}", 1, (255, 255, 255,))
         window.blit(text_lose, (10, 50))
 
+        bullets.update()
+
         ship.update()
         monsters.update()
 
         ship.reset()
         monsters.draw(window)
+        bullets.draw(window)
 
     display.update()
     Clock.tick(FPS)
